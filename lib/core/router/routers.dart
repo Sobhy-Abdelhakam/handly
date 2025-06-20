b6959becfefbd365163a72a33cc8247fc7213d03
Sobhy <sobhy.abdelhakam25@gmail.com>
2025-06-20 23:09:43 +0300

    define application screens

---
+++ lib/core/router/routers.dart
@@ -0,0 +1,22 @@
+class Routers {
+  static const String initial = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String forgetPassword = '/forget-password';
+  // bottom navigation
+  static const String main = '/main';
+  static const String home = '/home';
+  static const String cart = '/cart';
+  static const String favorites = '/favorites';
+  static const String profile = '/profile';
+
+  static const String search = '/search';
+  static const String notification = '/notification';
+  static const String productDetails = '/product';
+  static const String orderNow = '/order-now';
+  static const String orders = '/orders';
+  static const String orderDetails = '/order-details';
+  static const String addProduct = '/add-product';
+  static const String editProduct = '/edit-product';
+  static const String userProfile = '/user-profile';
+}
