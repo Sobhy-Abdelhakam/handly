1492bc494fd92d72d00614a1b3f8ed09c9fe2053
Sobhy <sobhy.abdelhakam25@gmail.com>
2025-06-22 15:50:31 +0300

    add responsive helper class

---
+++ lib/utils/responsive/responsive_helper.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveHelper {
+  static const double mobileBreakpoint = 600;
+  static const double tabletBreakpoint = 900;
+  static const double desktopBreakpoint = 1200;
+
+  static double screenWidth(BuildContext context) =>
+      MediaQuery.of(context).size.width;
+
+  static double screenHeight(BuildContext context) =>
+      MediaQuery.of(context).size.height;
+
+  static bool isMobile(BuildContext context) {
+    return screenWidth(context) < mobileBreakpoint;
+  }
+
+  static bool isTablet(BuildContext context) {
+    final width = screenWidth(context);
+    return width >= mobileBreakpoint && width < tabletBreakpoint;
+  }
+
+  static bool isDesktop(BuildContext context) {
+    return MediaQuery.of(context).size.width >= tabletBreakpoint;
+  }
+
+  static bool isPortrait(BuildContext context) =>
+      MediaQuery.of(context).orientation == Orientation.portrait;
+
+  static bool isLandscape(BuildContext context) =>
+      MediaQuery.of(context).orientation == Orientation.landscape;
+
+  static int getColumnCount(BuildContext context) {
+    if (isDesktop(context)) return 3;
+    if (isTablet(context)) return 2;
+    return 1;
+  }
+
+  static T getValue<T>(
+    BuildContext context, {
+    required T mobile,
+    T? tablet,
+    T? desktop,
+  }) {
+    if (isDesktop(context) && desktop != null) {
+      return desktop;
+    } else if (isTablet(context) && tablet != null) {
+      return tablet;
+    } else {
+      return mobile;
+    }
+  }
+
+  static EdgeInsets getPadding(BuildContext context) {
+    return EdgeInsets.all(
+      getValue(context, mobile: 16.0, tablet: 24.0, desktop: 32.0),
+    );
+  }
+
+  static double getFontSize(
+    BuildContext context, {
+    required double mobile,
+    double? tablet,
+    double? desktop,
+  }) {
+    return getValue(
+      context,
+      mobile: mobile,
+      tablet: tablet ?? mobile * 1.1,
+      desktop: desktop ?? mobile * 1.2,
+    );
+  }
+}
